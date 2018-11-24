c73bff058723e56088c62312b9004eaf6c58ecec
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-11-24 18:09:20 +0000

    User Model created

---
+++ src/users/models/User.js
@@ -0,0 +1,23 @@
+const mongoose =  require('mongoose')
+const Schema =  mongoose.Schema;
+
+//Desgin User Schema
+
+const UserSchema = new Schema({
+	name:{
+		type: String,
+		required: true
+	},
+	email:{
+		type: String,
+		required: true
+	},
+	password:{
+		type: String,
+		required: true
+	}
+})
+
+const User = mongoose.model('users', UserSchema)
+
+module.exports = User
