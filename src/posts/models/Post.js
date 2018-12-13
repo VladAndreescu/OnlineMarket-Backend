93461e505f6777d43dcdb81cd3061fbb39f361db
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-13 01:45:10 +0000

    Created mongoose Post Schema

---
+++ src/posts/models/Post.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+//Create Post Schema
+
+const PostSchema = new Schema({
+	user:{
+		type: Schema.Types.ObjectId,
+		ref: 'users'
+	},
+	name:{
+		type: String
+	},
+	category:{
+		type: String,
+		required: true
+	},
+	description:{
+		type: String,
+		required: true
+	},
+	item_condition:{
+		type: String,
+		required: true
+	},
+	asked_price:{
+		type: Number,
+		required: true
+	},
+	city:{
+		type: String,
+	},
+	likes:[
+		{
+			user: {
+				type: Schema.Types.ObjectId,
+				ref: 'users'
+			}
+		}
+	],
+	Bids:[
+		{
+			user: {
+				type: Schema.Types.ObjectId,
+				ref: 'users'
+			},
+			name:{
+				type: String
+			},
+			value:{
+				type: Number,
+				required: true
+			},
+			text:{
+				type:String
+			},
+			date:{
+				type: Date,
+				default: Date.now
+			}
+
+
+		}
+	],
+	date:{
+		type: Date,
+		default: Date.now
+	}
+
+	
+})
+
+Post = mongoose.model('posts', PostSchema)
+module.exports = Post
