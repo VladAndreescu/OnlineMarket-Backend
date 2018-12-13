936ea62e12e62599d88a27ee76e0f0bc1df919dc
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-13 00:22:01 +0000

    Implemented Log-in function.

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	MongoDbURI: 'mongodb://snyk:snyk123@ds239217.mlab.com:39217/304cem_database'
+	MongoDbURI: 'mongodb://snyk:snyk123@ds239217.mlab.com:39217/304cem_database',
+	secret: 'secretProgramming'
 }
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../config/keys')
+const passport = require('passport')
+
+
+//Load User Model
 const User = require('../src/users/models/User')
 
 
 const SALT_ROUNDS = 10
 router.get('/test', (req, res ) => res.json({msg: 'users router is working fine'}))
 
-//@route POST api/users/register
-//@desc Register an user
-//@acccess Public
+//@route 	POST api/users/register
+//@desc 	Register an user
+//@acccess 	Public
 router.post('/register', (req,res) =>{
 	User.findOne({email: req.body.email})
 		.then(user =>{
@@ -37,6 +43,44 @@ router.post('/register', (req,res) =>{
 
 
 			}
+		})
+})
+
+//@route 	POST api/users/login
+//@desc 	Login an existing user
+//@acccess 	Public
+router.post('/login', (req, res) =>{
+
+	// store the user inputs into variables
+	const email = req.body.email
+	const password = req.body.password
+
+	//check if the email exists in database
+	User.findOne({email})
+		.then(user =>{
+			if(user === null){
+				return res.status(404).json({user: 'User not found'})
+			}
+			//compare passwords in order to log in the user
+			bcrypt.compare(password, user.password)
+				.then(isMatch =>{
+					if(isMatch){
+
+						//User matched create payload
+						const payload = {id: user.id, name: user.name}
+
+						//Create the Sign Token - should expire in 1 hour for security reasons
+						jwt.sign(payload, keys.secret, {expiresIn: 3600}, (err, token) =>{
+							res.json({
+								success: true,
+								message: 'Log in successfuly!',
+								token: 'Bearer ' + token
+							})
+						})
+					}else{
+						return res.status(404).json({passwordError: 'Password incorrect'})
+					}
+				})
 		})
 })
 
