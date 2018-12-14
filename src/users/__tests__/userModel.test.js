34d5ccb4819fb36a596b43ff42217b9e47a65476
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-14 08:20:31 +0000

    Implemented Testing for SAVE, GET and UPDATE user

--- src/users/__tests__/userModel.test.js
+++ src/users/__tests__/userModel.test.js
@@ -21,4 +21,35 @@ describe('Test user model', () =>{
 		expect(User).toBeDefined()
 	})
 
+	describe('SAVE user', () => {
+		test('saves an user', async() => {
+			const newUser = new User({ name: 'Oana', email: 'oana@gmail.com', password: 'oana123'})
+			const savedUser = await newUser.save()
+			expect(savedUser.name).toEqual('Oana')
+			expect(savedUser.email).toEqual('oana@gmail.com')
+
+		})
+	})
+
+	describe('GET user', () => {
+		test('gets an user', async() => {
+			const newUser = new User({ name: 'Oana', email: 'oana@gmail.com', password: 'oana123'})
+			await newUser.save()
+
+			const foundUser = await User.findOne({ email: 'oana@gmail.com'})
+			expect(foundUser.email).toEqual('oana@gmail.com')
+			expect(foundUser.name).toEqual('Oana')
+		})
+	})
+
+	describe('UPDATE user', () => {
+		test('update an existing user', async() => {
+			const newUser = new User({ name: 'Oana', email: 'oana@gmail.com', password: 'oana123'})
+			await newUser.save()
+
+			newUser.email = 'oana123@gmail.com'
+			const updatedUser = await newUser.save()
+			expect(updatedUser.email).toBe('oana123@gmail.com')
+		})
+	})
 })
