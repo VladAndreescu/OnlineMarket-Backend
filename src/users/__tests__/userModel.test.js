8d50ab8ed3136afe033ec3afeb7312e813614460
VladAndreescu <andreesv@uni.coventry.ac.uk>
2018-12-14 08:14:37 +0000

    Test to check if the User has a model

--- src/users/__tests__/userModel.test.js
+++ src/users/__tests__/userModel.test.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose')
 const testUserDB = require('../../../config/keys').testUsersMongURI
-mongoose.connect(testUserDB)
+mongoose.connect('mongodb://snyk:snyk123@ds125402.mlab.com:25402/304cem-users-test-db')
 const User = require('../models/User')
 
 
 describe('Test user model', () =>{
-	
+	beforeAll( async() => {
+		await User.remove({})
+	})
+
+	afterEach( async() => {
+		await User.remove({})
+	})
+
+	afterAll( async() => {
+		await mongoose.connection.close()
+	})
+
+	test('has a model', () => {
+		expect(User).toBeDefined()
+	})
+
 })
